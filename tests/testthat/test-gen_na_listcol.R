2a8254bd7839ae3b85a87febdb90e7bc03e1a0e6
Gustavo Velásquez <gvelasq2github@gmail.com>
2025-08-20 00:19:45 +0800

    Add `gen_na_listcol()`

---
+++ tests/testthat/test-gen_na_listcol.R
@@ -0,0 +1,29 @@
+test_that("non-list objects fail", {
+  expect_error(gen_na_listcol(letters[1:3]))
+  expect_error(gen_na_listcol(1:3))
+})
+
+test_that("list-column NULLs are converted to NAs", {
+  library(dplyr)
+  a <-
+    mtcars %>%
+    select(cyl, vs, am) %>%
+    slice(1:6) %>%
+    as_tibble() %>%
+    mutate(listcol = list(NULL, "b", "c", "d", "e", "f")) %>%
+    mutate(across(starts_with("listcol"), gen_na_listcol))
+  expect_equal(a[["listcol"]][[1]], NA)
+})
+
+test_that("parallelization works", {
+  library(dplyr)
+  mirai::daemons(parallel::detectCores() - 1)
+  a <-
+    mtcars %>%
+    select(cyl, vs, am) %>%
+    slice(1:6) %>%
+    as_tibble() %>%
+    mutate(listcol = list(NULL, "b", "c", "d", "e", "f")) %>%
+    mutate(across(starts_with("listcol"), gen_na_listcol))
+  expect_equal(a[["listcol"]][[1]], NA)
+})
